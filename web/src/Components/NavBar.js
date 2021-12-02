225f6a4d682f8171201838b2cb84e8a8a34e409a
Lily <li.jiangy@northeastern.edu>
2021-12-02 14:18:58 -0800

    should work well now

--- web/src/Components/NavBar.js
+++ web/src/Components/NavBar.js
@@ -7,16 +7,16 @@ export const NavBar = () => {
       <div><br/>
         <Navbar position='sticky' bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="/JoshuaKazdan/home"><h1 >Joshua Kazdan</h1></Navbar.Brand>
+            <Navbar.Brand href="/JoshProfolilo"><h1 >Joshua Kazdan</h1></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-              <Nav.Link href='/'>home</Nav.Link>
-                <Nav.Link href='/JoshProfolilo/#/Teaching#Teaching' >Teaching</Nav.Link>
+              <Nav.Link href='/JoshProfolilo'>home</Nav.Link>
+                <Nav.Link href='/JoshProfolilo/#Teaching#Teaching' >Teaching</Nav.Link>
                 <NavDropdown title="Math/CS" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="/JoshProfolilo/#/Math#Math">Math Research</NavDropdown.Item>
-                  <NavDropdown.Item href="/JoshProfolilo/#/CS#CS">CS Experience</NavDropdown.Item>
-                  <NavDropdown.Item href="/JoshProfolilo/#/Hobbies#Hobbies">Hobbies</NavDropdown.Item>
+                  <NavDropdown.Item href="/JoshProfolilo/#Math#Math">Math Research</NavDropdown.Item>
+                  <NavDropdown.Item href="/JoshProfolilo/#CS#CS">CS Experience</NavDropdown.Item>
+                  <NavDropdown.Item href="/JoshProfolilo/#Hobbies#Hobbies">Hobbies</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="https://www.linkedin.com/in/joshua-kazdan-16ba49201/" target="_blank">My Linkedin</NavDropdown.Item>
                 </NavDropdown>
