837ab542891f10e66e593dc5279ebccec518ebef
Lily <li.jiangy@northeastern.edu>
2021-12-02 10:01:16 -0800

    replace browserrouter with hashrouter

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,8 @@ import {Math} from './Components/Math';
 import {CS} from './Components/CS';
 
 import {Hobby} from './Components/Hobby'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route, HashRouter, Switch} from 'react-router-dom';
+import React from "react";
 
 export const App = () => {
   return (
@@ -14,8 +15,26 @@ export const App = () => {
       <br/>
       <NavBar/>
       <br />
- 
-      <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/Teaching' element={<Teaching/>}></Route>
+          <Route path='/CS' element={<CS/>}></Route>
+          <Route path='/Math' element={<Math/>}></Route>
+          <Route path='/Hobbies' element={<Hobby/>}></Route>
+        </Routes>
+
+
+
+      {/* <React.StrictMode>
+        <HashRouter>
+        {/* <Route path='/' element={<Home/>}></Route>
+          <Route path='/Teaching' element={<Teaching/>}></Route>
+          <Route path='/CS' element={<CS/>}></Route>
+          <Route path='/Math' element={<Math/>}></Route>
+          <Route path='/Hobbies' element={<Hobby/>}></Route> */}
+        {/* </HashRouter>
+      </React.StrictMode> */}
+{/* <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/Teaching' element={<Teaching/>}></Route>
@@ -23,7 +42,7 @@ export const App = () => {
           <Route path='/Math' element={<Math/>}></Route>
           <Route path='/Hobbies' element={<Hobby/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter>  */}
       </div>
     
   );
--- web/src/index.js
+++ web/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <App/>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
